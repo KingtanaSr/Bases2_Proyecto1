5e182c69486016b064c877438146243be6249f5c
Josué Quintana. S <160699958+KingtanaSr@users.noreply.github.com>
2025-04-17 21:55:27 -0600

    Update app.js

--- Back/app.js
+++ Back/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const clientRoutes = require('./routes/clientRoutes');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/crearUsuarioRoutes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', clientRoutes);
+app.use('/api', authRoutes);
+app.use('/api', userRoutes)
 
 const PORT = 5000;
 app.listen(PORT, () => {
