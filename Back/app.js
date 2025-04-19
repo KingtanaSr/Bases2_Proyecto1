8469fa581109b69fdddd93ecdd635e1101a4ddae
Ariana Jiménez Paniagua <142464449+TheAriuu@users.noreply.github.com>
2025-04-18 21:32:58 -0600

    Add files via upload

--- Back/app.js
+++ Back/app.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const clientRoutes = require('./routes/clientRoutes');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/crearUsuarioRoutes');
+const proveedorProductoRoutes = require('./routes/proveedorProductoRoutes');
+const reportesRoutes = require('./routes/reportesRoutes');
 
 const app = express();
 app.use(cors());
@@ -10,6 +12,8 @@ app.use(express.json());
 app.use('/api', clientRoutes);
 app.use('/api', authRoutes);
 app.use('/api', userRoutes)
+app.use('/api', proveedorProductoRoutes);
+app.use('/api/reportes', reportesRoutes);
 
 const PORT = 5000;
 app.listen(PORT, () => {
