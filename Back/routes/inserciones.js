ec95effbd0b2b1dcf1f93b387129356b4462d162
Ariana Jiménez Paniagua <142464449+TheAriuu@users.noreply.github.com>
2025-04-18 21:37:10 -0600

    Delete Back/routes/inserciones.js

--- Back/routes/inserciones.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { insertPublication } = require('../controllers/insertController');
-
-// Se define la ruta para manejar la inserción de publicaciones
-router.post('/publicaciones', insertPublication);
-  
-module.exports = router;
